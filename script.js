b0fc8d2c3ccfd3a4cc5386fc9f62ee51555cb148
MT <221427704+inflammable252-bit@users.noreply.github.com>
2025-11-10 17:49:24 -0800

    Add div with button to change grid or return error Modify randomizer() for lighter, pastel colors

--- script.js
+++ script.js
@@ -1,40 +1,62 @@
-boxInput=16;
-boxCount = boxInput*boxInput
+let boxInput=16;
+let boxCount = boxInput * boxInput;
 canvas = 800;
 
 container = document.querySelector(".container")
 
-function addBox(count) {
-        for (x=1; x<=count; x++) {
-            const box = document.createElement("div");
-            box.classList.toggle("box");
-            container.appendChild(box);
-            box.style.width = ((canvas / boxInput) - 4) + "px";
-            box.style.height = ((canvas / boxInput) - 4) + "px";
-        }
-    }
+const gridInput = document.querySelector("#gridInput")
+const gridSubmit = document.querySelector("#gridSubmit")
 
 addBox(boxCount)
+
+gridInput.addEventListener("input", (e) => {
+return boxInput = e.target.value;
+})
+
+gridSubmit.addEventListener("click", () => {
+    errorText = document.querySelector("#error")
+    if (boxInput > 16) {
+        return errorText.textContent = "Maximum size: 100";
+    }
+    else {
+        errorText.textContent = ""
+    }
+   
+    let oldBoxes = document.querySelectorAll(".box")
+    oldBoxes.forEach((node) => node.remove())
+    boxCount = boxInput * boxInput;
+addBox(boxCount);
+})
+
+function addBox(count) {
+    for (x=1; x<=count; x++) {
+    const box = document.createElement("div");
+    box.classList.toggle("box");
+    container.appendChild(box);
+    box.style.width = ((canvas / boxInput) - 4) + "px";
+    box.style.height = ((canvas / boxInput) - 4) + "px";
+    }
+enableColors()
+}
+
+
 
 randomizer = () => {
     randomNum = () => {
         return String(
-            (Math.round(
-                (Math.random()*255)
-                )
-            )
-        )
+            (Math.round((Math.random()*190) + 65)))
     }
 
 return `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`
 }
 
-
-const boxes = document.querySelectorAll(".box");
-boxes.forEach((boxdiv) => boxdiv.addEventListener("mouseover", () => {
-    randomRGB = randomizer()
-    boxdiv.style.backgroundColor = randomRGB;
-}))
+function enableColors() {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((boxdiv) => boxdiv.addEventListener("mouseover", () => {
+        randomRGB = randomizer()
+        boxdiv.style.backgroundColor = randomRGB;
+    }))
+}
 
 console.log(randomizer())
 
